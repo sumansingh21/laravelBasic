a7934b94d247fecf9d51d479ba1c469c2a5bde5f
sumansingh21 <sumans.bca1821@gmail.com>
2025-03-11 09:43:02 +0530

    Error fixed

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,8 +17,8 @@
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul
                 class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-             <x-navbar-link herf="/" :active="request()->is('/')">Home</x-navbar-link>
-             <x-navbar-link herf="/posts" :active="request()->is('posts')">Posts</x-navbar-link>
+             <x-navbar-link href="/" :active="request()->is('/')">Home</x-navbar-link>
+             <x-navbar-link href="/posts" :active="request()->is('posts')">Posts</x-navbar-link>
             </ul>
         </div>
     </div>
